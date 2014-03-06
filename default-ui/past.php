af8f62b04ed6c9d69c0d3575d8d380e0d23f0ca9
Jshauk <gavincrenshaw95@gmail.com>
2014-03-06 15:27:24 -0500

    [ADD] icon to display how many FutureLinks there are connected to the past and when the icon is clicked, a table is displayed that shows metadata connected to it

--- default-ui/past.php
+++ default-ui/past.php
@@ -10,6 +10,8 @@
 	<script src="../Phraser/rangy-phraser.js"></script>
 	<script src="../scripts/FutureLink.js"></script>
 	<script src="../scripts/PastLink.js"></script>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 	<script>
         var flpData = <?php echo json_encode($pairs);?>,
             counts = <?php echo json_encode(FLP\PairAssembler::$counts) ?>,
@@ -28,18 +30,36 @@
 				return false;
 			});
 
-            var phrases = $('span.phrases');
+            var phrases = $('span.phrases'),
+                phrasesLookupTable = {},
+                show = function(table) {
+                    $('<div>')
+                        .append(table)
+                        .dialog();
+                };
 
-            for(var i = 0; i <= flpData.length; i++) {
-                new FutureLink(
+            for(var x = 0; x < flpData.length; x++){
+                if(!phrasesLookupTable[flpData[x].pastText.sanitized]){
+                    phrasesLookupTable[flpData[x].pastText.sanitized] = [];
+                }
+                phrasesLookupTable[flpData[x].pastText.sanitized].push(flpData[x]);
+            }
+
+
+            for(var i = 0; i < flpData.length; i++) {
+                var futureLink = new FutureLink(
                     phrases.filter('span.phraseBeginning' + i),
                     phrases.filter('span.phrase' + i),
                     phrases.filter('span.phraseEnd' + i),
-                    counts[flpData[i].pastText.sanitized]
+                    counts[flpData[i].pastText.sanitized],
+                    phrasesLookupTable[flpData[i].pastText.sanitized]
                 );
-            }
-		});
 
+                futureLink.show = show;
+            }
+
+
+		});
 	</script>
 </head>
 <body>
--- scripts/FutureLink.js
+++ scripts/FutureLink.js
@@ -1,10 +1,78 @@
-var FutureLink = function(beginning, middle, end, count) {
-    var phrase = this.phrase = beginning.add(middle).add(end);
-    this.beginning = beginning;
-    this.middle = middle;
-    this.end = end;
-    this.count = count;
+var FutureLink = (function() {
 
-    phrase.css('background-color', 'yellow');
-    $("<span>&amp;</span>").insertAfter(this.end);
-};
+    var Construct = function(beginning, middle, end, count, phrases) {
+        var that = this,
+            phrase = this.phrase = beginning.add(middle).add(end);
+        this.beginning = beginning;
+        this.middle = middle;
+        this.end = end;
+        this.count = count;
+        this.phrases = phrases;
+
+        var icon = this.icon = document.createElement('span');
+        icon.className = 'futureLink';
+        this.end.after(icon);
+        icon.onclick = function(){
+            var tab = document.createElement('table');
+            tab.setAttribute('border', '1px');
+            for(var i = 0; i < phrases.length; i++){
+                var auth = phrases[i].pair.past.author,
+                    prof = phrases[i].pair.past.authorProfession,
+                    site = phrases[i].pair.past.href,
+                    row1 = document.createElement('tr'),
+                    row2 = document.createElement('tr'),
+                    thead = document.createElement('thead'),
+                    head1 = document.createElement('th'),
+                    head2 = document.createElement('th'),
+                    head3 = document.createElement('th'),
+                    link = document.createElement('a'),
+                    col1 = document.createElement('td'),
+                    col2 = document.createElement('td'),
+                    col3 = document.createElement('td');
+                link.setAttribute('href', String(site));
+                head1.innerHTML = 'Author';
+                head2.innerHTML = 'Profession';
+                head3.innerHTML = 'Source';
+                tab.appendChild(thead);
+                tab.appendChild(row2);
+                thead.appendChild(row1);
+                row1.appendChild(head1);
+                row1.appendChild(head2);
+                row1.appendChild(head3);
+                row2.appendChild(col1);
+                row2.appendChild(col2);
+                row2.appendChild(col3);
+                col1.innerText = auth;
+                col2.innerText = prof;
+                col3.appendChild(link);
+                link.innerText = site;
+            }
+
+            that.show(tab);
+        };
+        switch(count){
+            case 1:
+                icon.innerHTML = '&#9676;';
+                break;
+            case 2:
+                icon.innerHTML = '&#9675;';
+                break;
+            case 3:
+                icon.innerHTML = '&#9678;';
+                break;
+            case 4:
+                icon.innerHTML = '&#9673;';
+                break;
+            default:
+                icon.innerHTML = '&#9679;';
+                break;
+        }
+        phrase.addClass('ui-state-highlight').css('border-width', '0px');
+    }
+
+    Construct.prototype = {
+        show: function (table) {}
+    };
+
+    return Construct;
+})();
